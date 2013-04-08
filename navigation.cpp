64ba6e0423346497b503a58692a130aea1768a71
Mathieu Cassard <cassard.mathieu@gmail.com>
2013-04-08 21:16:36 +0200

    First draft of gotoPoint function.
    Signed-off-by: Mathieu Cassard <cassard.mathieu@gmail.com>

---
+++ navigation.cpp
@@ -0,0 +1,32 @@
+#include "navigation.h"
+#include "utils.h"
+#include "conf.h"
+#include "asserv.h"
+#include "odo.h"
+#include "coders.h"
+
+int nav_gotoPoint(double new_x, double new_y, double delta_max)
+{
+	double old_x=odo_X;
+	double old_y=odo_Y;
+	double diff_x = new_x-old_x;
+	double diff_y = new_y-old_y;
+
+	double approx_dist = pythagore(diff_x,diff_y);
+
+	if(approx_dist>=delta_max)
+	{
+		double new_angle = atan2(diff_y,diff_x);
+		if(diff_x<0) //cos is < 0
+			new_angle=-new_angle;
+		asserv_setTarget(
+			odo_meters2ticks(approx_dist),
+			odo_rads2ticks(new_angle),
+			DEST_REL|ANGL_ABS);
+		
+		return -1;		
+	}else{
+		return 0;
+	}
+
+}
---
+++ navigation.h
@@ -0,0 +1,9 @@
+#ifndef NAVIGATION_H
+#define NAVIGATION_H
+
+#include <Arduino.h> 
+
+int nav_gotoPoint(double new_x, double new_y, double delta_max);
+
+
+#endif
